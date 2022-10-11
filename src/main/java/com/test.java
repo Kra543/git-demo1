23b3468751803b9ad6ea1787f0c5e6ade5230ce5
liuchikun <927489621@qq.com>
2022-10-11 10:15:33 +0800

    first commit

---
+++ src/main/java/com/test.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * author: chikun liu
+ * time  : 2022/10/11 10:13
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
